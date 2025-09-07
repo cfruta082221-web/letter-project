157ddedcba98859cbdb856b82cfd67d0b0d00446
cfruta082221-web <cfruta082221@gmail.com>
2025-09-08 03:01:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const message = `Hi bubbby, I know its too late and cringe pajud pag approach but I want u to know that I really miss you. 
+const message = `Hey bab I know its too late and cringe pajud pag approach but I want u to know that I really miss you. 
 Taga gabie ko sig huna huna nimo and I really want it to stop kay dinako gusto ma damgohan ka kay sakit. 
 Stuck sa what could've beens and what if and what should've been sa atong relationship. 
 I don't think I can find someone better or someone who I can love the way I loved you hart. 
 You bring a piece of me nga pangitaon sa uban. 
-I love you hart and I miss u, sa damgo magunitan taka usab and maka uban, sakit siya pero thats the only time I can feel you. 
+I love you and I miss you, sa damgo magunitan taka usab and maka uban sakit siya pero thats the only time I can feel you. 
 I'd rather feel that pain, that delusion, than not feeling anything from you ever again. 
-I dont wanna miss the chance. And I hope I can still see you and talk to you again. 
+I don't wanna miss the chance. And I hope I can still see you and talk to you again. 
 I miss you very much.`;  
 
 function showLetter() {
@@ -22,7 +22,11 @@ function showLetter() {
       if (i < message.length) {
         typedText.innerHTML += message.charAt(i);
         i++;
-        setTimeout(typeWriter, 30);
+
+        // 📱 Auto-scroll as text appears
+        letterBox.scrollTop = letterBox.scrollHeight;
+
+        setTimeout(typeWriter, 30); // typing speed
       }
     }
 
